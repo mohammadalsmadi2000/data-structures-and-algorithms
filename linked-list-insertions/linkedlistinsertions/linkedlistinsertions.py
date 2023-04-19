65e50863f326634da5c36f91d8d8dfe7c25ecc34
mohammadalsmadi2000 <mohammadalsmadi.fcbayern@gmail.com>
2023-04-19 15:20:13 +0000

    add docString

--- linked-list-insertions/linkedlistinsertions/linkedlistinsertions.py
+++ linked-list-insertions/linkedlistinsertions/linkedlistinsertions.py
@@ -1,13 +1,35 @@
 class Node:
+    """
+    Represents a single node of a linked list.
+    
+    Attributes:
+        value (Any): The value of the node.
+        next (Node): The reference to the next node.
+    """
     def __init__(self, value=None):
         self.value = value
         self.next = None
 
 class LinkedList:
+    """
+    Represents a singly linked list data structure.
+    
+    Attributes:
+        head (Node): The reference to the first node of the list.
+    """
     def __init__(self):
+        """
+        Initializes an empty linked list.
+        """
         self.head = None
 
     def append(self, value):
+        """
+        Appends a new node with the given value to the end of the list.
+        
+        Args:
+            value (Any): The value to be added to the list.
+        """
         new_node = Node(value)
 
         if not self.head:
@@ -19,6 +41,16 @@ class LinkedList:
             current.next = new_node
 
     def insert_before(self, value, new_value):
+        """
+        Inserts a new node with the given value before the node with the specified value.
+        
+        Args:
+            value (Any): The value of the existing node.
+            new_value (Any): The value of the new node to be inserted.
+        
+        Raises:
+            Exception: If the list is empty or the specified value is not found.
+        """
         if not self.head:
             raise Exception("List is empty")
 
@@ -40,6 +72,16 @@ class LinkedList:
         raise Exception(f"Node with value {value} not found")
 
     def insert_after(self, value, new_value):
+        """
+        Inserts a new node with the given value after the node with the specified value.
+        
+        Args:
+            value (Any): The value of the existing node.
+            new_value (Any): The value of the new node to be inserted.
+        
+        Raises:
+            Exception: If the list is empty or the specified value is not found.
+        """
         if not self.head:
             raise Exception("List is empty")
 
@@ -55,6 +97,15 @@ class LinkedList:
         raise Exception(f"Node with value {value} not found")
 
     def delete(self, value):
+        """
+        Deletes the first node with the specified value from the list.
+        
+        Args:
+            value (Any): The value of the node to be deleted.
+        
+        Raises:
+            Exception: If the list is empty or the specified value is not found.
+        """
         if not self.head:
             raise Exception("List is empty")
 
