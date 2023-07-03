8cabf8790152ac3c4047c20eff952807e1b947fa
mohammadalsmadi2000 <mohammadalsmadi.fcbayern@gmail.com>
2023-07-03 11:57:14 +0000

    refactor: 🔒 Implement enhanced hashing algorithms

--- Hash/hash.py
+++ Hash/hash.py
@@ -1,7 +1,7 @@
 class Hashtable:
     def __init__(self, size=10):
         self.size = size
-        self.table = [[] for _ in range(size)]
+        self.table = [{} for _ in range(size)]
 
     def hash(self, key):
         return hash(key) % self.size
@@ -9,31 +9,17 @@ class Hashtable:
     def set(self, key, value):
         index = self.hash(key)
         bucket = self.table[index]
-        for i, (k, v) in enumerate(bucket):
-            if k == key:
-                bucket[i] = (key, value)
-                return
-        bucket.append((key, value))
+        bucket[key] = value
 
     def get(self, key):
         index = self.hash(key)
         bucket = self.table[index]
-        for k, v in bucket:
-            if k == key:
-                return v
-        return None
+        return bucket.get(key)
 
     def has(self, key):
         index = self.hash(key)
         bucket = self.table[index]
-        for k, _ in bucket:
-            if k == key:
-                return True
-        return False
+        return key in bucket
 
     def keys(self):
-        keys = []
-        for bucket in self.table:
-            for k, _ in bucket:
-                keys.append(k)
-        return keys
+        return [key for bucket in self.table for key in bucket.keys()]
