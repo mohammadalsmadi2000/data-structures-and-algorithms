fa689fe9eba8cc8176d0a42e9396c1d757bb6347
mohammadalsmadi2000 <mohammadalsmadi.fcbayern@gmail.com>
2023-07-17 18:23:05 +0000

    test: ✅ Add test case for graph breadth first

--- graph/tests/test_graph.py
+++ graph/tests/test_graph.py
@@ -13,8 +13,8 @@ def test_add_edge():
     graph.add_edge(vertex1, vertex2, 5)
     neighbors = graph.get_neighbors(vertex1)
     assert len(neighbors) == 1
-    assert neighbors[0].vertex == vertex2
-    assert neighbors[0].weight == 5
+    assert neighbors[0][0] == vertex2  
+    assert neighbors[0][1] == 5
 
 def test_get_vertices():
     graph = Graph()
@@ -34,10 +34,10 @@ def test_get_neighbors():
     graph.add_edge(vertex1, vertex3, 3)
     neighbors = graph.get_neighbors(vertex1)
     assert len(neighbors) == 2
-    assert neighbors[0].vertex == vertex2
-    assert neighbors[0].weight == 5
-    assert neighbors[1].vertex == vertex3
-    assert neighbors[1].weight == 3
+    assert neighbors[0][0] == vertex2
+    assert neighbors[0][1] == 5
+    assert neighbors[1][0] == vertex3
+    assert neighbors[1][1] == 3
 
 def test_neighbors_with_weight():
     graph = Graph()
@@ -46,8 +46,8 @@ def test_neighbors_with_weight():
     graph.add_edge(vertex1, vertex2, 5)
     neighbors = graph.get_neighbors(vertex1)
     assert len(neighbors) == 1
-    assert neighbors[0].vertex == vertex2
-    assert neighbors[0].weight == 5
+    assert neighbors[0][0] == vertex2
+    assert neighbors[0][1] == 5
 
 def test_size():
     graph = Graph()
@@ -63,8 +63,36 @@ def test_graph_with_one_vertex_and_edge():
     graph.add_edge(vertex1, vertex1, 10)
     edges = graph.get_neighbors(vertex1)
     assert len(edges) == 2
-    assert edges[0].vertex == vertex1
-    assert edges[0].weight == 10
-    assert graph.size() == 1
+    assert edges[0][0] == vertex1
+    assert edges[0][1] == 10
+    assert edges[1][0] == vertex1
+    assert edges[1][1] == 10
 
 
+def test_breadth_first():
+    graph = Graph()
+
+  
+    pandora = graph.add_vertex("Pandora")
+    arendelle = graph.add_vertex("Arendelle")
+    metroville = graph.add_vertex("Metroville")
+    monstroplolis = graph.add_vertex("Monstroplolis")
+    narnia = graph.add_vertex("Narnia")
+    naboo = graph.add_vertex("Naboo")
+
+    
+    graph.add_edge(pandora, arendelle, 4)
+    graph.add_edge(pandora, metroville, 7)
+    graph.add_edge(arendelle, metroville, 3)
+    graph.add_edge(arendelle, monstroplolis, 8)
+    graph.add_edge(metroville, monstroplolis, 6)
+    graph.add_edge(metroville, narnia, 2)
+    graph.add_edge(metroville, naboo, 5)
+    graph.add_edge(monstroplolis, naboo, 9)
+    graph.add_edge(narnia, naboo, 10)
+
+    
+    result = graph.breadth_first(pandora)
+    expected_output = ["Pandora", "Arendelle", "Metroville", "Monstroplolis", "Narnia", "Naboo"]
+
+    assert result == expected_output
